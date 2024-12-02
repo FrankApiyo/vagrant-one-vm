cde53bb6e9d5e22288464505c0fb58c7164b1aab
FrankApiyo <franklineapiyo@gmail.com>
2024-12-02 09:38:40 +0300

    Allow access to port 80

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -8,6 +8,14 @@ resource "aws_security_group" "ssh_access" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "Allow HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow SSH access"
     from_port   = 22
     to_port     = 22
